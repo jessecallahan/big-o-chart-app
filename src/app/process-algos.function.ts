b915611a2c9b8afd47e41f6bb485ec6d489919ea
jcallahan <jesse.callahan@thinkfellow.com>
2023-10-13 09:42:40 -0700

    Add to readme

--- src/app/process-algos.function.ts
+++ src/app/process-algos.function.ts
@@ -45,7 +45,7 @@ const logLinear = function logLinear(n: number, operations = 0) {
   let y = n;
   while(n > 1) {
     n = Math.floor(n / 2)
-    for (let i = 1; i<= y; i++) {
+    for (let i = 1; i <= y; i++) {
       operations++;
     }
   }
